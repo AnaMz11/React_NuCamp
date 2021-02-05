d76b3d7148798aab4d9ee00538a445da1de9272c
Anamz <anayansi.l.munoz@gmail.com>
2021-02-04 20:18:50 -0800

    breadcrumbs

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,6 +4,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
+import CampsiteInfo from './CampsiteInfoComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { CAMPSITES } from '../shared/campsites';
 import { COMMENTS } from '../shared/comments';
@@ -33,14 +34,14 @@ class Main extends Component {
             );
         };
 
-        // const CampsiteWithId = ({match}) => {
-        //     return (
-        //         <CampsiteInfo 
-        //             campsite={this.state.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
-        //             comments={this.state.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
-        //         />
-        //     );
-        // };    
+        const CampsiteWithId = ({match}) => {
+            return (
+                <CampsiteInfo 
+                    campsite={this.state.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
+                    comments={this.state.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
+                />
+            );
+        };    
  
 
         return (
@@ -49,7 +50,7 @@ class Main extends Component {
                     <Switch>
                         <Route path='/home' component={HomePage} />
                         <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
-                        {/* <Route path='/directory/:campsiteId' component={CampsiteWithId} /> */}
+                        <Route path='/directory/:campsiteId' component={CampsiteWithId} />
                         <Route exact path='/contactus' component={Contact} />
                         <Redirect to='/home' />
                     </Switch>
